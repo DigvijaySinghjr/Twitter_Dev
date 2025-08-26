0535fe545c40eadcef2176a31979773df2e1b85a
DigvijaySinghjr <neymar9838jr@gmail.com>
2025-08-26 15:24:43 +0530

    added tweet services with hashtag processing

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const connect = require('./config/database');
+const { HashtagRepository } = require('./repository');
+const TweetService = require('./services/tweet-service');
 const app = express();
-
-const TweetRepository = require('./repository/tweet-repository');
-const Comment = require('./models/comment');
 
 app.listen(3000, async () => {
     console.log('server started');
     await connect();
     console.log('Mongo db connected');
-    // const tweet = await Tweet.create({
-    //     content: 'Third tweet',
-    // });
-    // const tweets = await Tweet.find({userEmail: 'a@b.com'});
-    const tweetRepo = new TweetRepository();
-    const tweet = await tweetRepo.getAll(0, 4);
-    console.log(tweet[0].contentWithEmail);
+    let service = new TweetService();
+    const tweet = await service.create({
+         content: ' #love this this is girl my #first tweet #lethargic #netflix&chill   #joyous' 
+        });
+
 });
---
+++ src/services/tweet-service.js
@@ -0,0 +1,33 @@
+const { TweetRepository, HashtagRepository }  = require('../repository/index');
+
+class TweetService {
+    constructor() {
+        this.tweetRepository = new TweetRepository();
+        this.hashtagRepository = new HashtagRepository();
+    }
+    
+    async create(data) {
+        const content = data.content;
+        const tags = content.match(/#\w+/g).map((tag) => tag.substring(1));   //this regex extracts hashtags
+        const tweet = await this.tweetRepository.create(data);
+        let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
+        let titleOfPresentTags =  alreadyPresentTags.map(tags => tags.title);
+        let newTags = tags.filter(tag => !titleOfPresentTags.includes(tag));
+        newTags = newTags.map(tag => {
+            return {title: tag, tweets: [tweet.id]}
+        });
+        await this.hashtagRepository.bulkcreate(newTags);
+        alreadyPresentTags.forEach((tag) =>{
+            tag.tweets.push(tweet.id);
+            tag.save();
+        });
+        return tweet;
+    }
+}
+
+module.exports = TweetService;
+
+
+/*
+this is my #first #tweet . I am really #excited
+*/

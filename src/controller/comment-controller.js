4d04f8d46c4f5b1c1d75d2a568ed14529bb79b3c
DigvijaySinghjr <neymar9838jr@gmail.com>
2025-09-06 09:32:58 +0530

    protected comment creation
    - Removed req.body.userId
    
    - Using req.userID from verified JWT payload

--- src/controller/comment-controller.js
+++ src/controller/comment-controller.js
@@ -5,7 +5,8 @@ const commentService = new CommentService();
 export const createComment = async(req, res) => {
     try {
         console.log('hit commentService');
-        const response = await commentService.create(req.body.userId, req.query.modelId, req.query.modelType, req.body.content);
+        const response = await commentService.create(req.body.userId, req.query.modelId, req.query.modelType, req.body.content);    
+        //instead of req.body.userId, we should use req.userId (in above line)
         return res.status(200).json({
             success: true,
             message: 'Successfully fetched a tweet',
@@ -21,3 +22,22 @@ export const createComment = async(req, res) => {
         });
     }
 }
+
+
+/**
+ * Both work because of different data sources:
+1) req.user.id - From JWT authentication
+-> Set by your authenticate middleware after verifying the token
+-> Contains the user ID from the JWT payload
+-> Secure - can't be faked by client
+
+2) req.body.userId - From request body
+-> Sent by the client in the JSON payload
+-> Insecure - client can send any user ID they want
+-> Security risk - users could comment as other users
+
+Why req.user.id is better:
+-> Authentication-based - uses the verified token
+-> Secure - can't be manipulated by client
+-> Consistent - always matches the logged-in user
+ */
--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,9 +1,9 @@
-import passporot from 'passport';
+import passport from 'passport';
 
 export const authenticate = (req, res, next) => {
     passport.authenticate('jwt', (err, user) => {
         if(err) next(err);
-        if(!err) {
+        if(!user) {
             return res.status(401).json({
                 message: 'Unauthorized access no token'
             })
